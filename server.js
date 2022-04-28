47a5400c9bcfad4865158cb2ffc94c5b342c762c
tomerlevyg <47150256+tomerlevyg@users.noreply.github.com>
2022-04-28 21:01:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello #########################################@@@');
+  res.send('Hello Marina');
 });
 
 app.listen(PORT, HOST);

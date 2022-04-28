11bcde798a33af86df020f2f6551825f3fab733f
Tomer Levy <tl1996@gmail.com>
2022-04-28 21:01:16 +0300

    updated

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+git pull --dry-run | grep -q -v 'Already up-to-date.' || exit 1
 git pull
 docker build -t hello-world:latest .
 docker rm -f marina
 docker run -d -p 8999:80 --name marina hello-world:latest
+
